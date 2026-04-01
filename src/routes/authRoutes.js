2b0f5e790a15916e58ea97559cc883f47afae6cc
omtaywade9@gmail.com <omtaywade9@gmail.com>
2026-04-01 18:49:16 +0530

    error 2  arise due to src

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const upload = require('../utils/upload');
 
-const authController = require('../../controllers/authController');
+const authController = require('../controllers/authController');
 
 //middleware
 const { isLoggedIn } = require('../middleware/authMiddleware');
