779b7956a24d12f97db96686eef047ed8287f8d4
omtaywade9@gmail.com <omtaywade9@gmail.com>
2026-04-01 18:15:41 +0530

    connectdb changes

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
-const { connectdb } = require('./config/db');
+const connectdb = require('./config/db');
 connectdb();
 
 const session = require('express-session');
