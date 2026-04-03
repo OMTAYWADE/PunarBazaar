ab5f0f44313cecd4319e1a4a0faea27d96ba80c6
omtaywade9@gmail.com <omtaywade9@gmail.com>
2026-04-03 12:27:08 +0530

    redis-server 3

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,7 +1,7 @@
 const { createClient } = require('redis');
 
 const client = createClient({
-    url: "redis://localhost:6379"
+    url: "redis://127.0.0.1:6379"
 });
 
 client.on("error", (err) => console.log('redis Error', err));
