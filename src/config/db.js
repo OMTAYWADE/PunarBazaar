4f4b293559bafbfa2d63665c9cc289777e5cd15e
omtaywade9@gmail.com <omtaywade9@gmail.com>
2026-04-24 20:45:28 +0530

    db.js changes

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         console.log("MONGO_URI:", process.env.MONGO_URI);
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect("mongodb+srv://Anonymous_market:Trust_Build@cluster0.5evh6y7.mongodb.net/?appName=punarbazaar");
 
         console.log("✅ MongoDB Connected");
         console.log("DB NAME:", conn.connection.name); // ✅ CORRECT
