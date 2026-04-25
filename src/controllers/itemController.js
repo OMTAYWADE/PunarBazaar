ca33f5a8e2e959979ac9e20c81c756a370eb2021
omtaywade9@gmail.com <omtaywade9@gmail.com>
2026-04-25 13:31:38 +0530

    feature is missing in controller

--- src/controllers/itemController.js
+++ src/controllers/itemController.js
@@ -122,6 +122,32 @@ exports.getSearchItems = async (req, res, next) => {
     }
 }
 
+exports.featureItem = async (req, res) => {
+    try {
+        const item = await Item.findById(req.params.id);
+
+        if (!item) {
+            return res.status(404).json({ success: false, message: "Item not found" });
+        }
+
+        if (item.user.toString() !== req.user.userId) {
+            return res.status(403).json({ success: false, message: "Not authorized" });
+        }
+
+        const expiry = new Date();
+        expiry.setDate(expiry.getDate() + 2);
+
+        await Item.findByIdAndUpdate(req.params.id, {
+            isFeatured: true,
+            featuredUntil: expiry,
+        });
+
+        res.redirect('/');
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message });
+    }
+};
+
 // create razor pay order
 exports.createOrder = async (req, res) => {
       try {
